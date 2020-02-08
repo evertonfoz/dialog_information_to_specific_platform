cb963abb9cfdc836bc3019aad5bbd7b07ec21496
evertonfoz <evertoncoimbra@gmail.com>
2020-02-08 18:04:13 -0300

    1804

---
+++ example/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:dialog_information_to_specific_platform/dialog_information_to_specific_platform.dart';
+import 'package:dialog_information_to_specific_platform/flat_buttons/actions_flatbutton_to_alert_dialog.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue,
+          child: Icon(Icons.message),
+          elevation: 10,
+          onPressed: () async {
+            await showDialog(
+              barrierDismissible: false,
+              context: context,
+              child: InformationAlertDialog(
+                iconTitle: Icon(
+                  Icons.error,
+                  color: Colors.red,
+                ),
+                title: 'Não conseguimos',
+                message: 'Conteúdo',
+                buttons: [
+                  ActionsFlatButtonToAlertDialog(
+                    messageButton: 'Cancelar',
+                  ),
+                  //   InformationAlertDialog.createFlatButton(),
+                ],
+              ),
+            );
+          }),
+    );
+  }
+}
